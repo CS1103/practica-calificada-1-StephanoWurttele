f5546248bf11bc866e4d2090a4d3fdea2315f0e9
StephanoWurttele <39769681+StephanoWurttele@users.noreply.github.com>
2019-04-12 13:35:11 -0500

    Update TwoLinkedList.h

--- PC1/TwoLinkedList.h
+++ PC1/TwoLinkedList.h
@@ -4,11 +4,36 @@
 
 #ifndef PC1_TWOLINKEDLIST_H
 #define PC1_TWOLINKEDLIST_H
+#include <string>
+#include <iostream>
+#include <fstream>
 
+namespace UTEC{
+    struct Node{
+        int value;
+        Node* next;
+        Node(int _value);
+    };
 
-class TwoLinkedList {
-
-};
+    class TwoLinkedList {
+        Node* head1;
+        Node* head2;
+        Node* head3;
+        Node* tail1;
+        Node* tail2;
+        Node* tail;
+    public:
+        TwoLinkedList();
+        ~TwoLinkedList();
+        void push_back_1(int value);
+        void push_back_2(int value);
+        std::string merge(int merged);
+        bool is_merged();
+        std::string getlist(int lista);
+        void search(int buscado);
+        void save(std::string archivo);
+    };
+}
 
 
 #endif //PC1_TWOLINKEDLIST_H
